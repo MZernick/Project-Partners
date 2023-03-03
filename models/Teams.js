8cceb10ea149ad45c38549f91e3c9dedb7e80f88
Dominique Nix <raquel_0216@yahoo.com>
2023-03-03 14:14:30 -0600

    adding models

---
+++ models/Teams.js
@@ -0,0 +1,50 @@
+const { Schema, model } = require('mongoose');
+
+
+const teamSchema = new Schema(
+    {
+        // title of the team you are creating
+        title: {
+            type: String,
+            required: true,
+            trim: true,
+          },
+        //   short description fo purpose of the team?
+        descrption: {
+            type: String,
+            required: true, 
+            trim: true
+        },
+        // date team was created
+        createdAt: {
+          type: Date,
+          get: ((date) => {
+              date = new Date().toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"}) 
+              return date
+          }),
+          default: Date.now,
+      },
+        // array of objects that referfences the users model to track users on team
+        members: [
+            {
+              type: Schema.Types.ObjectId, 
+              ref: 'User',
+            },
+        ],
+    },
+    {
+      toJSON: {
+        getters: true,
+        virtuals: true,
+      },
+      id: false,
+    }
+)
+
+teamSchema.virtual('memberCount').get(function () {
+    return this.members.length;
+  });
+
+  const Team = model('team', teamSchema);
+
+  module.exports = Team;
---
+++ models/User.js
@@ -0,0 +1,64 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            match:[/.+@.+\..+/, 'Must match an email address!'],
+        },
+        password: {
+            type: String,
+            required: true, 
+            minlength: 5,
+        },
+        personality: {
+            type:String,
+            // required: true,
+        },
+        teams: [
+            {
+              type: Schema.Types.ObjectId, 
+              ref: 'Teams',
+            },
+        ],
+    },
+    {
+      toJSON: {
+        virtuals: true,
+      },
+      id: false,
+    }
+)
+// retrieves the length of the user's teams (might be helpful somewhere)
+userSchema.virtual('teamCount').get(function () {
+    return this.teams.length;
+  });
+
+// set up pre-save middleware to create password
+profileSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// compare the incoming password with the hashed password
+profileSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = model('user', userSchema);
+
+module.exports = User;

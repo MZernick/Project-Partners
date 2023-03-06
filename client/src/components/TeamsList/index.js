c1f65e3dcc72a589ce1b696cdbbd292d10f42d5b
Moonbeam <mfzernick@gmail.com>
2023-03-05 18:23:07 -0600

    added profile page with teamlsit component

---
+++ client/src/components/TeamsList/index.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { useMutation } from '@apollo/client';
+//be sure to create the mutations for this
+import { REMOVE_TEAM } from '../../utils/mutations';
+
+//be sure to create the query for this
+import { QUERY_ME } from '../../utils/queries';
+
+const TeamList = ({ teams, isLoggedInUser = false }) => {
+  const [removeTeam, { error }] = useMutation(REMOVE_TEAM, {
+    update(cache, { data: { removeTeam } }) {
+      try {
+        cache.writeQuery({
+          query: QUERY_ME,
+          data: { me: removeTeam },
+        });
+      } catch (e) {
+        console.error(e);
+      }
+    },
+  });
+
+  const handleRemoveTeam = async (team) => {
+    try {
+      const { data } = await removeTeam({
+        variables: { team },
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  if (!teams.length) {
+    return <h3>No Teams Yet</h3>;
+  }
+
+  return (
+    <div>
+      <div className="flex-row justify-space-between my-4">
+        {teams &&
+          teams.map((team) => (
+            <div key={team} className="col-12 col-xl-6">
+              <div className="card mb-3">
+                <h4 className="card-header bg-dark text-light p-2 m-0 display-flex align-center">
+                  <span>{team.title}</span>
+                  {isLoggedInUser && (
+                    <button
+                      className="btn btn-sm btn-danger ml-auto"
+                      onClick={() => handleRemoveTeam(team)}
+                    >
+                      X
+                    </button>
+                  )}
+                </h4>
+              </div>
+            </div>
+          ))}
+      </div>
+      {error && (
+        <div className="my-3 p-3 bg-danger text-white">{error.message}</div>
+      )}
+    </div>
+  );
+};
+
+export default TeamList;
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,11 +1,67 @@
-import React from "react";
+import React from 'react';
 
-function Profile() {
+import { Navigate, useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+
+import TeamList from '../components/TeamList';
+import Pairings from '../components/Pairings';
+
+//profile query here- are we adding pairs/partners/favorites or just seeing partners in the their teamsview?
+import { QUERY_SINGLE_PROFILE, QUERY_ME } from '../utils/queries';
+
+import Auth from '../utils/auth';
+
+const Profile = () => {
+  const { userId } = useParams();
+
+  // If there is no `userId` in the URL as a parameter, execute the `QUERY_ME` query instead for the logged in user's information
+  const { loading, data } = useQuery(
+    userId ? QUERY_SINGLE_PROFILE : QUERY_ME,
+    {
+      variables: { userId: userId },
+    }
+  );
+
+  // Check if data is returning from the `QUERY_ME` query, then the `QUERY_SINGLE_PROFILE` query
+  const profile = data?.me || data?.user || {};
+
+  // Use React Router's `<Navigate />` component to redirect to personal profile page if username is yours
+  if (Auth.loggedIn() && Auth.getProfile().data._id === userId) {
+    return <Navigate to="/me" />;
+  }
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!user?.name) {
     return (
-        <div>
-            Profile
-        </div>
-    )
-}
+      <h4>
+        You need to be logged in to see your profile page. Use the navigation
+        links above to sign up or log in!
+      </h4>
+    );
+  }
+
+  return (
+    <div>
+      <h2 className="card-header">
+        {userId ? `${user.username}'s` : 'Your'} 
+      </h2>
+    <p>has a ${user.personality} type.</p>
+    <h4>Teams:</h4>
+      {user.teams?.length > 0 && (
+        <TeamList
+          teams={user.teams}
+          isLoggedInUser={!userId && true}
+        />
+      )}
+
+      <div className="my-4 p-4" style={{ border: '1px dotted #1a1a1a' }}>
+        <Pairings userId={user._id} />
+      </div>
+    </div>
+  );
+};
 
 export default Profile;

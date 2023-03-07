c5e32043aa9df9a95e5fbac5a56e5e92b6590aad
leyden <fusionsoccer05@gmail.com>
2023-03-06 20:08:34 -0600

    complete overhaul of the homepage. Added and changes js files

--- client/src/components/PortfolioContainer.js
+++ client/src/components/PortfolioContainer.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import NavTabs from './NavTabs';
 import Home from '../pages/Home';
+import Hometest from '../pages/Hometest';
 import Profile from '../pages/Profile';
 import MyTeams from '../pages/MyTeams';
 import JoinTeam from '../pages/JoinTeam';
@@ -11,8 +12,8 @@ export default function PortfolioContainer() {
 
   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
   const renderPage = () => {
-    if (currentPage === 'Home') {
-      return <Home />;
+    if (currentPage === 'Hometest') {
+      return <Hometest />;
     }
     if (currentPage === 'Profile') {
       return <Profile />;
@@ -26,7 +27,7 @@ export default function PortfolioContainer() {
     if (currentPage === 'Signup') {
         return <Signup />;
       }
-    return <Home />;
+    return <Hometest />;
   };
 
   const handlePageChange = (page) => setCurrentPage(page);

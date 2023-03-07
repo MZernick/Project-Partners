53b4fa228e05a09c5070c2f0ef42ecebaf380f42
AB <anitabanhtx@gmail.com>
2023-03-06 21:25:06 -0600

    package json corrections

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // add db name and mongoDB path in place of static ip
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://drn:1601@cluster0.224mut0.mongodb.net/project-partners', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://root:newpassword@utacoding.l3ykbwu.mongodb.net/project-partners', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });

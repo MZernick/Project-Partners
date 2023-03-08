dd4bf44a182bd0f030b6d484e6d4bf89f8e020d2
joshuagoeke <113941639+joshuagoeke@users.noreply.github.com>
2023-03-07 21:33:27 -0600

    starting teamStats.js experiements, also fixing missing INFJ in switch

---
+++ project-partners/joshdumbfolder/teamStats.js
@@ -0,0 +1,651 @@
+const data ={
+    "data": {
+      "users": [
+        {
+          "_id": "6407f0732cb481594884ed42",
+          "username": "BrianKernighan",
+          "personality": "INFJ",
+          "compatibility": [
+            {
+              "type": "INFP",
+              "rating": 4
+            },
+            {
+              "type": "ENFP",
+              "rating": 5
+            },
+            {
+              "type": "INFJ",
+              "rating": 4
+            },
+            {
+              "type": "ENFJ",
+              "rating": 4
+            },
+            {
+              "type": "INTJ",
+              "rating": 4
+            },
+            {
+              "type": "ENTJ",
+              "rating": 4
+            },
+            {
+              "type": "INTP",
+              "rating": 4
+            },
+            {
+              "type": "ENTP",
+              "rating": 5
+            },
+            {
+              "type": "ISFP",
+              "rating": 1
+            },
+            {
+              "type": "ESFP",
+              "rating": 1
+            },
+            {
+              "type": "ISTP",
+              "rating": 1
+            },
+            {
+              "type": "ESTP",
+              "rating": 1
+            },
+            {
+              "type": "ISFJ",
+              "rating": 1
+            },
+            {
+              "type": "ESFJ",
+              "rating": 1
+            },
+            {
+              "type": "ISTJ",
+              "rating": 1
+            },
+            {
+              "type": "ESTJ",
+              "rating": 1
+            }
+          ]
+        },
+        {
+          "_id": "6407f0732cb481594884ed43",
+          "username": "MaxKanat-Alexander",
+          "personality": "ENTJ",
+          "compatibility": [
+            {
+              "type": "INFP",
+              "rating": 5
+            },
+            {
+              "type": "ENFP",
+              "rating": 4
+            },
+            {
+              "type": "INFJ",
+              "rating": 4
+            },
+            {
+              "type": "ENFJ",
+              "rating": 4
+            },
+            {
+              "type": "INTJ",
+              "rating": 4
+            },
+            {
+              "type": "ENTJ",
+              "rating": 4
+            },
+            {
+              "type": "INTP",
+              "rating": 5
+            },
+            {
+              "type": "ENTP",
+              "rating": 4
+            },
+            {
+              "type": "ISFP",
+              "rating": 3
+            },
+            {
+              "type": "ESFP",
+              "rating": 3
+            },
+            {
+              "type": "ISTP",
+              "rating": 3
+            },
+            {
+              "type": "ESTP",
+              "rating": 3
+            },
+            {
+              "type": "ISFJ",
+              "rating": 3
+            },
+            {
+              "type": "ESFJ",
+              "rating": 3
+            },
+            {
+              "type": "ISTJ",
+              "rating": 3
+            },
+            {
+              "type": "ESTJ",
+              "rating": 3
+            }
+          ]
+        },
+        {
+          "_id": "6407f0732cb481594884ed44",
+          "username": "Amiko",
+          "personality": "INFP",
+          "compatibility": []
+        },
+        {
+          "_id": "6407f0732cb481594884ed45",
+          "username": "KentBeck",
+          "personality": "ESFP",
+          "compatibility": [
+            {
+              "type": "INFP",
+              "rating": 1
+            },
+            {
+              "type": "ENFP",
+              "rating": 1
+            },
+            {
+              "type": "INFJ",
+              "rating": 1
+            },
+            {
+              "type": "ENFJ",
+              "rating": 5
+            },
+            {
+              "type": "INTJ",
+              "rating": 3
+            },
+            {
+              "type": "ENTJ",
+              "rating": 3
+            },
+            {
+              "type": "INTP",
+              "rating": 3
+            },
+            {
+              "type": "ENTP",
+              "rating": 3
+            },
+            {
+              "type": "ISFP",
+              "rating": 2
+            },
+            {
+              "type": "ESFP",
+              "rating": 2
+            },
+            {
+              "type": "ISTP",
+              "rating": 2
+            },
+            {
+              "type": "ESTP",
+              "rating": 2
+            },
+            {
+              "type": "ISFJ",
+              "rating": 3
+            },
+            {
+              "type": "ESFJ",
+              "rating": 5
+            },
+            {
+              "type": "ISTJ",
+              "rating": 3
+            },
+            {
+              "type": "ESTJ",
+              "rating": 5
+            }
+          ]
+        },
+        {
+          "_id": "6407f0732cb481594884ed46",
+          "username": "EdwardV.Berard",
+          "personality": "ESTP",
+          "compatibility": [
+            {
+              "type": "INFP",
+              "rating": 1
+            },
+            {
+              "type": "ENFP",
+              "rating": 1
+            },
+            {
+              "type": "INFJ",
+              "rating": 1
+            },
+            {
+              "type": "ENFJ",
+              "rating": 1
+            },
+            {
+              "type": "INTJ",
+              "rating": 3
+            },
+            {
+              "type": "ENTJ",
+              "rating": 3
+            },
+            {
+              "type": "INTP",
+              "rating": 3
+            },
+            {
+              "type": "ENTP",
+              "rating": 3
+            },
+            {
+              "type": "ISFP",
+              "rating": 2
+            },
+            {
+              "type": "ESFP",
+              "rating": 2
+            },
+            {
+              "type": "ISTP",
+              "rating": 2
+            },
+            {
+              "type": "ESTP",
+              "rating": 2
+            },
+            {
+              "type": "ISFJ",
+              "rating": 5
+            },
+            {
+              "type": "ESFJ",
+              "rating": 3
+            },
+            {
+              "type": "ISTJ",
+              "rating": 5
+            },
+            {
+              "type": "ESTJ",
+              "rating": 3
+            }
+          ]
+        },
+        {
+          "_id": "6407f0732cb481594884ed47",
+          "username": "AlanKay",
+          "personality": "ENFJ",
+          "compatibility": [
+            {
+              "type": "INFP",
+              "rating": 5
+            },
+            {
+              "type": "ENFP",
+              "rating": 4
+            },
+            {
+              "type": "INFJ",
+              "rating": 4
+            },
+            {
+              "type": "ENFJ",
+              "rating": 4
+            },
+            {
+              "type": "INTJ",
+              "rating": 4
+            },
+            {
+              "type": "ENTJ",
+              "rating": 4
+            },
+            {
+              "type": "INTP",
+              "rating": 4
+            },
+            {
+              "type": "ENTP",
+              "rating": 4
+            },
+            {
+              "type": "ISFP",
+              "rating": 5
+            },
+            {
+              "type": "ESFP",
+              "rating": 1
+            },
+            {
+              "type": "ISTP",
+              "rating": 1
+            },
+            {
+              "type": "ESTP",
+              "rating": 1
+            },
+            {
+              "type": "ISFJ",
+              "rating": 1
+            },
+            {
+              "type": "ESFJ",
+              "rating": 1
+            },
+            {
+              "type": "ISTJ",
+              "rating": 1
+            },
+            {
+              "type": "ESTJ",
+              "rating": 1
+            }
+          ]
+        },
+        {
+          "_id": "6407f0732cb481594884ed48",
+          "username": "C.A.R.Hoare",
+          "personality": "ISTJ",
+          "compatibility": [
+            {
+              "type": "INFP",
+              "rating": 1
+            },
+            {
+              "type": "ENFP",
+              "rating": 1
+            },
+            {
+              "type": "INFJ",
+              "rating": 1
+            },
+            {
+              "type": "ENFJ",
+              "rating": 1
+            },
+            {
+              "type": "INTJ",
+              "rating": 2
+            },
+            {
+              "type": "ENTJ",
+              "rating": 3
+            },
+            {
+              "type": "INTP",
+              "rating": 2
+            },
+            {
+              "type": "ENTP",
+              "rating": 2
+            },
+            {
+              "type": "ISFP",
+              "rating": 3
+            },
+            {
+              "type": "ESFP",
+              "rating": 5
+            },
+            {
+              "type": "ISTP",
+              "rating": 3
+            },
+            {
+              "type": "ESTP",
+              "rating": 5
+            },
+            {
+              "type": "ISFJ",
+              "rating": 4
+            },
+            {
+              "type": "ESFJ",
+              "rating": 4
+            },
+            {
+              "type": "ISTJ",
+              "rating": 4
+            },
+            {
+              "type": "ESTJ",
+              "rating": 4
+            }
+          ]
+        },
+        {
+          "_id": "6407f0732cb481594884ed49",
+          "username": "DavidThomas",
+          "personality": "ESTJ",
+          "compatibility": [
+            {
+              "type": "INFP",
+              "rating": 1
+            },
+            {
+              "type": "ENFP",
+              "rating": 1
+            },
+            {
+              "type": "INFJ",
+              "rating": 1
+            },
+            {
+              "type": "ENFJ",
+              "rating": 1
+            },
+            {
+              "type": "INTJ",
+              "rating": 2
+            },
+            {
+              "type": "ENTJ",
+              "rating": 3
+            },
+            {
+              "type": "INTP",
+              "rating": 5
+            },
+            {
+              "type": "ENTP",
+              "rating": 2
+            },
+            {
+              "type": "ISFP",
+              "rating": 5
+            },
+            {
+              "type": "ESFP",
+              "rating": 3
+            },
+            {
+              "type": "ISTP",
+              "rating": 5
+            },
+            {
+              "type": "ESTP",
+              "rating": 3
+            },
+            {
+              "type": "ISFJ",
+              "rating": 4
+            },
+            {
+              "type": "ESFJ",
+              "rating": 4
+            },
+            {
+              "type": "ISTJ",
+              "rating": 4
+            },
+            {
+              "type": "ESTJ",
+              "rating": 4
+            }
+          ]
+        },
+        {
+          "_id": "6407f0732cb481594884ed4a",
+          "username": "DonaldKnuth",
+          "personality": "ENFJ",
+          "compatibility": [
+            {
+              "type": "INFP",
+              "rating": 5
+            },
+            {
+              "type": "ENFP",
+              "rating": 4
+            },
+            {
+              "type": "INFJ",
+              "rating": 4
+            },
+            {
+              "type": "ENFJ",
+              "rating": 4
+            },
+            {
+              "type": "INTJ",
+              "rating": 4
+            },
+            {
+              "type": "ENTJ",
+              "rating": 4
+            },
+            {
+              "type": "INTP",
+              "rating": 4
+            },
+            {
+              "type": "ENTP",
+              "rating": 4
+            },
+            {
+              "type": "ISFP",
+              "rating": 5
+            },
+            {
+              "type": "ESFP",
+              "rating": 1
+            },
+            {
+              "type": "ISTP",
+              "rating": 1
+            },
+            {
+              "type": "ESTP",
+              "rating": 1
+            },
+            {
+              "type": "ISFJ",
+              "rating": 1
+            },
+            {
+              "type": "ESFJ",
+              "rating": 1
+            },
+            {
+              "type": "ISTJ",
+              "rating": 1
+            },
+            {
+              "type": "ESTJ",
+              "rating": 1
+            }
+          ]
+        },
+        {
+          "_id": "6407f0732cb481594884ed4b",
+          "username": "Lernantino",
+          "personality": "ISTP",
+          "compatibility": [
+            {
+              "type": "INFP",
+              "rating": 1
+            },
+            {
+              "type": "ENFP",
+              "rating": 1
+            },
+            {
+              "type": "INFJ",
+              "rating": 1
+            },
+            {
+              "type": "ENFJ",
+              "rating": 1
+            },
+            {
+              "type": "INTJ",
+              "rating": 3
+            },
+            {
+              "type": "ENTJ",
+              "rating": 3
+            },
+            {
+              "type": "INTP",
+              "rating": 3
+            },
+            {
+              "type": "ENTP",
+              "rating": 3
+            },
+            {
+              "type": "ISFP",
+              "rating": 2
+            },
+            {
+              "type": "ESFP",
+              "rating": 2
+            },
+            {
+              "type": "ISTP",
+              "rating": 2
+            },
+            {
+              "type": "ESTP",
+              "rating": 2
+            },
+            {
+              "type": "ISFJ",
+              "rating": 3
+            },
+            {
+              "type": "ESFJ",
+              "rating": 5
+            },
+            {
+              "type": "ISTJ",
+              "rating": 3
+            },
+            {
+              "type": "ESTJ",
+              "rating": 5
+            }
+          ]
+        }
+      ]
+    }
+  }

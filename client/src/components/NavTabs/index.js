79970df5da10192bfd8dd663ddfd74f148379547
leyden <fusionsoccer05@gmail.com>
2023-03-11 13:58:20 -0600

    mostly navbar stuff

--- client/src/components/NavTabs/index.js
+++ client/src/components/NavTabs/index.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import '../../styles/Nav.css';
+import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
+import GroupsRoundedIcon from '@mui/icons-material/GroupsRounded';
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+import AddCircleOutlineRoundedIcon from '@mui/icons-material/AddCircleOutlineRounded';
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
 
 function NavTabs({ currentPage, handlePageChange }) {
   return (
     <nav>
     <ul className="nav">
+      <li id="logoPlaceholder">
+        <p>
+          INSERT LOGO HERE
+        </p>
+      </li>
       <li>
         <a id="nav-link"
           href="/"
           onClick={() => handlePageChange('Home')}
           className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
         >
-          Home
-        </a>
-      </li>
-      <li >
-        <a id="nav-link"
-          href="/user/:userId"
-          onClick={() => handlePageChange('Profile')}
-          className={currentPage === 'Profile' ? 'nav-link active' : 'nav-link'}
-        >
-          Profile
+          <HomeRoundedIcon fontSize="large" id="icon1" />
         </a>
       </li>
       <li>
@@ -29,7 +30,7 @@ function NavTabs({ currentPage, handlePageChange }) {
           onClick={() => handlePageChange('MyTeams')}
           className={currentPage === 'MyTeams' ? 'nav-link active' : 'nav-link'}
         >
-          My Teams
+           <GroupsRoundedIcon fontSize="large" id="icon2"/>
         </a>
       </li>
       <li>
@@ -38,15 +39,25 @@ function NavTabs({ currentPage, handlePageChange }) {
           onClick={() => handlePageChange('UserSearch')}
           className={currentPage === 'UserSearch' ? 'nav-link active' : 'nav-link'}
         >
-          Search Users
+           <SearchRoundedIcon fontSize="large" id="icon3"/>
           </a>
-          </li><li>
+          </li>
+          <li>
         <a id="nav-link"
           href="/createteam"
           onClick={() => handlePageChange('CreateTeam')}
           className={currentPage === 'CreateTeam' ? 'nav-link active' : 'nav-link'}
         >
-        Create Team
+         <AddCircleOutlineRoundedIcon fontSize="large" id="icon4"/>
+        </a>
+      </li>
+      <li >
+        <a id="nav-link"
+          href="/user/:userId"
+          onClick={() => handlePageChange('Profile')}
+          className={currentPage === 'Profile' ? 'nav-link active' : 'nav-link'}
+        >
+           <AccountCircleRoundedIcon fontSize="large" id="icon5"/>
         </a>
       </li>
     </ul>

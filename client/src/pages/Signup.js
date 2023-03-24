f436a0dcae11de2df908dcabe1a664bbe50d5db4
Moonbeam <mfzernick@gmail.com>
2023-03-24 16:17:41 -0500

    signup and login page now mobile friendly

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -46,7 +46,7 @@ const Signup = () => {
 
   const handleLogin = async (event) => {
     event.preventDefault();
-    
+
     try {
       const { data } = await login({
         variables: { ...loginState },
@@ -68,7 +68,7 @@ const Signup = () => {
   // submit form
   const handleSignup = async (event) => {
     event.preventDefault();
-    
+
 
     try {
       const { data } = await addUser({
@@ -85,164 +85,156 @@ const Signup = () => {
   return (
     <main>
       <div className="signup-page">
-        <div className="column">
-          <div className="signup-card">
-            <div>
-              <div className="headers">
-                <h4>Sign Up</h4>
-                <div className="underline-title"></div>
-              </div>
-              <div className="container">
-                {data ? (
-                  <p>
-                    Success! You may now head{" "}
-                    <Link to="/">back to the homepage.</Link>
-                  </p>
-                ) : (
-                  <form className="sign-up-form" onSubmit={handleSignup}>
-                    <label htmlFor="username"> Enter Username:</label>
-                    <input
-                      id="username"
-                      className="signupform-input"
-                      placeholder="Your username"
-                      name="username"
-                      type="text"
-                      value={signupState.name}
-                      onChange={handleSignupChange}
-                    />
-                    <div className="form-border"></div>
-                    <label htmlFor="email"> Enter Email:</label>
-                    <input
-                      id="email"
-                      className="signupform-input"
-                      placeholder="Your email"
-                      name="email"
-                      type="email"
-                      value={signupState.email}
-                      onChange={handleSignupChange}
-                    />
-                    <div className="form-border"></div>
-                    <label htmlFor="password">Enter Password:</label>
-                    <input
-                      id="password"
-                      className="signupform-input password-signup"
-                      placeholder="******"
-                      name="password"
-                      type="password"
-                      value={signupState.password}
-                      onChange={handleSignupChange}
-                    />
-                    <div className="form-border"></div>
-                    <label htmlFor="personality-type">
-                      Input your Myers-Briggs Personality Type Here:
-                    </label>
-                    <select
-                      id="personality-type"
-                      className="signupform-input"
-                      placeholder="Pick Personality Type"
-                      name="personality"
-                      value={signupState.personality}
-                      onChange={handleSignupChange}
-                    >
-                      <option value={null}>Enter Type</option>
-                      <option value="ENFJ">ENFJ</option>
-                      <option value="ENFP">ENFP</option>
-                      <option value="ENTJ">ENTJ</option>
-                      <option value="ENTP">ENTP</option>
-                      <option value="ESFJ">ESFJ</option>
-                      <option value="ESFP">ESFP</option>
-                      <option value="ESTJ">ESTJ</option>
-                      <option value="ESTP">ESTP</option>
-                      <option value="INFJ">INFJ</option>
-                      <option value="INFP">INFP</option>
-                      <option value="INTJ">INTJ</option>
-                      <option value="INTP">INTP</option>
-                      <option value="ISFJ">ISFJ</option>
-                      <option value="ISFP">ISFP</option>
-                      <option value="ISTJ">ISTJ</option>
-                      <option value="ISTP">ISTP</option>
-                    </select>
-                    <div className="form-border"></div>
-                    <button
-                      className="btn btn-block btn-info signup-btn"
-                      style={{ cursor: "pointer" }}
-                      type="submit"
-                    >
-                      Submit
-                    </button>
-                    <p>
-                      <label>
-                        Don't know your personality type? Take this quick quiz:
-                      </label>{" "}
-                      <a
-                        id="ptest"
-                        rel="noreferrer"
-                        href="https://www.16personalities.com/free-personality-test"
-                        target="_blank"
-                      >
-                        16personalites.com
-                      </a>
-                    </p>
-                  </form>
-                )}
+        <div className="signup-card">
+          <div className="headers">
+            <h4>Sign Up</h4>
+            <div className="underline-title"></div>
+          </div>
+          <div className="container">
+            {data ? (
+              <p>
+                Success! You may now head{" "}
+                <Link to="/">back to the homepage.</Link>
+              </p>
+            ) : (
+              <form className="sign-up-form" onSubmit={handleSignup}>
+                <label htmlFor="username"> Enter Username:</label>
+                <input
+                  id="username"
+                  className="signupform-input"
+                  placeholder="Your username"
+                  name="username"
+                  type="text"
+                  value={signupState.name}
+                  onChange={handleSignupChange}
+                />
+                <div className="form-border"></div>
+                <label htmlFor="email"> Enter Email:</label>
+                <input
+                  id="email"
+                  className="signupform-input"
+                  placeholder="Your email"
+                  name="email"
+                  type="email"
+                  value={signupState.email}
+                  onChange={handleSignupChange}
+                />
+                <div className="form-border"></div>
+                <label htmlFor="password">Enter Password:</label>
+                <input
+                  id="password"
+                  className="signupform-input password-signup"
+                  placeholder="******"
+                  name="password"
+                  type="password"
+                  value={signupState.password}
+                  onChange={handleSignupChange}
+                />
+                <div className="form-border"></div>
+                <label htmlFor="personality-type">
+                  Input your Myers-Briggs Personality Type Here:
+                </label>
+                <select
+                  id="personality-type"
+                  className="signupform-input"
+                  placeholder="Pick Personality Type"
+                  name="personality"
+                  value={signupState.personality}
+                  onChange={handleSignupChange}
+                >
+                  <option value={null}>Enter Type</option>
+                  <option value="ENFJ">ENFJ</option>
+                  <option value="ENFP">ENFP</option>
+                  <option value="ENTJ">ENTJ</option>
+                  <option value="ENTP">ENTP</option>
+                  <option value="ESFJ">ESFJ</option>
+                  <option value="ESFP">ESFP</option>
+                  <option value="ESTJ">ESTJ</option>
+                  <option value="ESTP">ESTP</option>
+                  <option value="INFJ">INFJ</option>
+                  <option value="INFP">INFP</option>
+                  <option value="INTJ">INTJ</option>
+                  <option value="INTP">INTP</option>
+                  <option value="ISFJ">ISFJ</option>
+                  <option value="ISFP">ISFP</option>
+                  <option value="ISTJ">ISTJ</option>
+                  <option value="ISTP">ISTP</option>
+                </select>
+                <div className="form-border"></div>
+                <button
+                  className="btn btn-block btn-info signup-btn"
+                  style={{ cursor: "pointer" }}
+                  type="submit"
+                >
+                  Submit
+                </button>
+                <p>
+                  <label>
+                    Don't know your personality type? Take this quick quiz:
+                  </label>{" "}
+                  <a
+                    id="ptest"
+                    rel="noreferrer"
+                    href="https://www.16personalities.com/free-personality-test"
+                    target="_blank"
+                  >
+                    16personalites.com
+                  </a>
+                </p>
+              </form>
+            )}
 
-                {error && (
-                  <div className="my-3 p-3 bg-danger text-white">
-                    {error.message}
-                  </div>
-                )}
+            {error && (
+              <div className="my-3 p-3 bg-danger text-white">
+                {error.message}
               </div>
-            </div>
+            )}
           </div>
         </div>
-        <div className="column">
-          <div className="login-card">
-            <div>
-              <div className="headers">
-                <h4>Login</h4>
-                <div className="underline-title"></div>
-              </div>
-              <div className="container">
-                {data1 ? (
-                  <p>
-                    Success! You may now head{" "}
-                    <Link to="/">back to the homepage.</Link>
-                  </p>
-                ) : (
-                  <form className="sign-up-form" onSubmit={handleLogin}>
-                    <input
-                      className="signupform-input"
-                      placeholder="Your email"
-                      name="email"
-                      type="email"
-                      value={loginState.email}
-                      onChange={handleLoginChange}
-                    />
-                    <div className="form-border"></div>
-                    <input
-                      className="signupform-input"
-                      placeholder="******"
-                      name="password"
-                      type="password"
-                      value={loginState.password}
-                      onChange={handleLoginChange}
-                    />
-                    <div className="form-border"></div>
-                    <button
-                      className="btn btn-block btn-info login-btn"
-                      style={{ cursor: "pointer" }}
-                      type="submit"
-                    >
-                      Submit
-                    </button>
-                  </form>
-                )}
+        <div className="login-card">
+          <div className="headers">
+            <h4>Login</h4>
+            <div className="underline-title"></div>
+          </div>
+          <div className="container">
+            {data1 ? (
+              <p>
+                Success! You may now head{" "}
+                <Link to="/">back to the homepage.</Link>
+              </p>
+            ) : (
+              <form className="sign-up-form" onSubmit={handleLogin}>
+                <input
+                  className="signupform-input"
+                  placeholder="Your email"
+                  name="email"
+                  type="email"
+                  value={loginState.email}
+                  onChange={handleLoginChange}
+                />
+                <div className="form-border"></div>
+                <input
+                  className="signupform-input"
+                  placeholder="******"
+                  name="password"
+                  type="password"
+                  value={loginState.password}
+                  onChange={handleLoginChange}
+                />
+                <div className="form-border"></div>
+                <button
+                  className="btn btn-block btn-info login-btn"
+                  style={{ cursor: "pointer" }}
+                  type="submit"
+                >
+                  Submit
+                </button>
+              </form>
+            )}
 
-                {error1 && (
-                  <div className="my-3 p-3 bg-danger text-white">{error}</div>
-                )}
-              </div>
-            </div>
+            {error1 && (
+              <div className="my-3 p-3 bg-danger text-white">{error}</div>
+            )}
           </div>
         </div>
       </div>
